c9725c354617a23268b36abf9965850f264dcc8c
Richard Gabaree <richard.gabaree@gmail.com>
2021-02-02 17:24:23 -0600

    fixed issue with notes page popping up

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
 const fs = require("fs");
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -9,7 +9,7 @@ app.use(express.json());
 //connects the css and js files from the puplic folder
 app.use(express.static("public"));
 
-// require("./routes/apiRoutes")(app);
+require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 
