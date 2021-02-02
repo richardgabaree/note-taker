59b8532374b7369d9e0f5e861bc833a0eea935ac
Richard Gabaree <richard.gabaree@gmail.com>
2021-02-01 18:48:32 -0600

    updted server.js

--- public/server.js
+++ public/server.js
@@ -1,30 +1,19 @@
 const express = require("express");
-
+const fs = require("fs");
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extends: true }));
 app.use(express.json());'git pushgit '
 //connects the css and js files from the puplic folder
 app.use(express.static("public"));
 
-//Routes
-module.exports = function (app) {
-  // Basic route that sends the user first to the AJAX Page
-  app.get("/", (req, res) =>
-    res.sendFile(path.join(__dirname, "../index.html"))
-  );
 
-  app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../notes.html"));
-    res.json(notes);
-  });
+require("./routes/apiRoute")(app);
+require("./routes/htmlRoute")(app);
 
-  app.post("/notes", (req, res) => {
-    const newNote = req.body;
-  });
 
-  const newLocal = app.listen(PORT, function () {
-    console.log(`Server is listening on PORT: ${PORT}`);
+app.listen(PORT, function() {
+    console.log("App listening on: http://localhost:" + PORT);
   });
-};
