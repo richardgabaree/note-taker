fa4108aedd1f8a81c699dec0464e6397718913ed
Richard Gabaree <richard.gabaree@gmail.com>
2021-02-01 20:02:45 -0600

    set up api routes for index and notes html

---
+++ routes/apiroutes.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.export = function(app) {
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'))
+        });
+
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'))
+        });
+}
