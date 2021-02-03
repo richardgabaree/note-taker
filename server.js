ef8213cd49741bf58e4fae606584dbdd2cdb319e
Richard Gabaree <richard.gabaree@gmail.com>
2021-02-02 23:21:10 -0600

    updated

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(express.json());
 //connects the css and js files from the puplic folder
 app.use(express.static("public"));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routes/apiRoute")(app);
+require("./routes/htmlRoute")(app);
 
 
 app.listen(PORT, function() {
-    console.log("App listening on: http://localhost:" + PORT);
+    console.log(`App listening on: PORT:${PORT}`);
   });
